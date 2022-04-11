1a591b667d19dd4f9534747711279c76ec96f2d6
xJaSzyy <94278083+xJaSzyy@users.noreply.github.com>
2022-04-11 12:27:04 +0700

    Update DiscordBot.cs

--- DiscordBot.cs
+++ DiscordBot.cs
@@ -17,17 +17,42 @@ namespace DiscordBot
         {
             Client = new DiscordSocketClient();
             Client.MessageReceived += CommandHandler;
+            Client.Log += Log;
 
-            var Token = "OTYyOTIxODUwNzkzNTA0Nzg4.YlOk2w.M4NrFyvMvcPcXIV3gS9j_NdfFjM";
+            var Token = "OTYyOTIxODUwNzkzNTA0Nzg4.YlOk2w.DMSk2oBM7aOkFHpUXhmNf5qS5iU";
             await Client.LoginAsync(TokenType.Bot, Token);
             await Client.StartAsync();
 
             Console.ReadLine();
         }
-
-        private Task CommandHandler(SocketMessage arg)
+        private Task Log(LogMessage msg)
         {
-            throw new NotImplementedException();
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
         }
+
+        private Task CommandHandler(SocketMessage msg)
+        {
+            if (!msg.Author.IsBot)
+            {
+                switch (msg.Content)
+                {
+                    case "!help": 
+                        {
+                            msg.Channel.SendMessageAsync("1 - !Привет\n2 - !.\n3 - !.");
+                            break;
+                        }
+                    case "!Привет":
+                        {
+                            msg.Channel.SendMessageAsync($"Привет, {msg.Author}");
+                            break;
+                        }
+                }
+            }
+            return Task.CompletedTask;
+            
+        }
+
+        
     }
 }
